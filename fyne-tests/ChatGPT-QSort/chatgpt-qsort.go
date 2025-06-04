09a0adfe598cf02dd546ffa38f23acbf07c44634
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2025-06-04 22:11:03 +0100

    Adding new test project! woo hoo! Done by ChaGPT

---
+++ fyne-tests/ChatGPT-QSort/chatgpt-qsort.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	//	"sort"
+	"sync"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+const dataSize = 50
+const goroutineThreshold = 5
+
+var (
+	data  []int
+	rects []*canvas.Rectangle
+	mutex sync.Mutex
+	wg    sync.WaitGroup
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Goroutine QuickSort Visualisation")
+	w.Resize(fyne.NewSize(800, 400))
+
+	// rand.Seed(time.Now().UnixNano()) // deprecated
+	data = rand.Perm(dataSize)
+
+	// Create the rectangles to represent data
+	rects = make([]*canvas.Rectangle, dataSize)
+	bars := make([]fyne.CanvasObject, dataSize)
+	for i := range data {
+		rect := canvas.NewRectangle(theme.Color(theme.ColorNamePrimary))
+		height := float32(data[i]+1) * 5
+		rect.SetMinSize(fyne.NewSize(10, height))
+		rects[i] = rect
+		bars[i] = rect
+	}
+
+	barContainer := container.NewHBox(bars...)
+	btn := widget.NewButton("Sort!", func() {
+		go func() {
+			QuickSort(0, len(data)-1)
+			wg.Wait()
+			fmt.Println("Sorting done!")
+		}()
+	})
+
+	layout := container.NewBorder(nil, btn, nil, nil, barContainer)
+	w.SetContent(layout)
+	w.ShowAndRun()
+}
+
+func QuickSort(low, high int) {
+	if low < high {
+		pivot := partition(low, high)
+
+		if (high - low) > goroutineThreshold {
+			wg.Add(2)
+			go func() {
+				defer wg.Done()
+				QuickSort(low, pivot-1)
+			}()
+			go func() {
+				defer wg.Done()
+				QuickSort(pivot+1, high)
+			}()
+		} else {
+			QuickSort(low, pivot-1)
+			QuickSort(pivot+1, high)
+		}
+	}
+}
+
+func partition(low, high int) int {
+	pivot := data[high]
+	i := low - 1
+
+	for j := low; j < high; j++ {
+		if data[j] < pivot {
+			i++
+			data[i], data[j] = data[j], data[i]
+			updateUI(i, j)
+		}
+	}
+
+	data[i+1], data[high] = data[high], data[i+1]
+	updateUI(i+1, high)
+	return i + 1
+}
+
+func updateUI(i, j int) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	fyne.CurrentApp().SendNotification(&fyne.Notification{Title: "Swap", Content: fmt.Sprintf("Swapped %d and %d", i, j)})
+
+	rects[i].FillColor = theme.Color(theme.ColorNameWarning)
+	rects[j].FillColor = theme.Color(theme.ColorNameError)
+	rects[i].Refresh()
+	rects[j].Refresh()
+
+	rects[i].SetMinSize(fyne.NewSize(10, float32(data[i]+1)*5))
+	rects[j].SetMinSize(fyne.NewSize(10, float32(data[j]+1)*5))
+
+	time.Sleep(50 * time.Millisecond)
+
+	rects[i].FillColor = theme.Color(theme.ColorNamePrimary)
+	rects[j].FillColor = theme.Color(theme.ColorNamePrimary)
+	rects[i].Refresh()
+	rects[j].Refresh()
+}
--- tinifier.go
+++ tinifier.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io"
+//	"io"
 	"os"
-	"time"
+//	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -14,40 +14,18 @@ import (
 	//	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
+//	"fyne.io/fyne/v2/widget"
 )
-
-// UI elements
-
-func updateTime(clock *widget.Label) {
-	formatted := time.Now().Format("Time: 03:04:05")
-	clock.SetText(formatted)
-}
 
 //
 func main() {
 	a := app.New()
-	wClock := a.NewWindow("Clock")
-
-	clock := widget.NewLabel("")
-	updateTime(clock)
-
-	wClock.SetContent(clock)
-	go func() {
-		for range time.Tick(time.Second) {
-			updateTime(clock)
-		}
-	}()
-	wClock.Show()
 
 	wMain := a.NewWindow(os.Args[0])
 	wMain.Resize(fyne.NewSize(800.0, 600.0))
 
 	image := canvas.NewImageFromResource(theme.FyneLogo())
-	// image := canvas.NewImageFromURI(uri)
-	// image := canvas.NewImageFromImage(src)
-	// image := canvas.NewImageFromReader(reader, name)
-	// image := canvas.NewImageFromFile(fileName)
+
 	image.SetMinSize(fyne.NewSize(800.0, 600.0))
 	image.FillMode = canvas.ImageFillOriginal
 	wMain.SetContent(image)
@@ -63,7 +41,6 @@ func main() {
 			return
 		}
 		fmt.Printf("chosen: %v\n", f.URI())
-		// loadFile(wMain, f, fileLength(f.URI().Path()))
 		image = canvas.NewImageFromURI(f.URI()) // TODO work with library upstream to not do this
 		image.FillMode = canvas.ImageFillOriginal
 		wMain.SetContent(image)
@@ -72,45 +49,9 @@ func main() {
 
 	d := dialog.NewFileOpen(onChosen, wMain)
 	d.SetFilter(storage.NewExtensionFileFilter([]string{".jpg", ".jpeg", ".webp", ".png", ".avif"}))
-	// d.Resize(fyne.NewSize(300.0, 200.0))
 	d.Show()
 
 	wMain.Show()
 
 	a.Run()
-}
-
-// additional functions
-
-// Returns the file length as an int64, or zero if file not found.
-func fileLength(path string) int64 {
-	f, err := os.Open(path)
-	if err != nil {
-		return 0
-	}
-	defer f.Close()
-
-	info, err := f.Stat()
-	if err != nil {
-		return 0
-	}
-
-	return info.Size()
-}
-
-func loadFile(parent fyne.Window, r io.ReadCloser, length int64) {
-	var data []byte
-	bytes, err := r.Read(data)
-	if err != nil {
-		dialog.ShowError(err, parent)
-		return
-	}
-
-	_ = r.Close()
-
-	if bytes < int(length) {
-		dialog.ShowError(fmt.Errorf("%d out of expected %d byte(s) read! File may be corrupt", bytes, length), parent)
-		return
-	}
-	dialog.ShowInformation("Status", fmt.Sprintf("%d byte(s) read", bytes), parent)
 }
